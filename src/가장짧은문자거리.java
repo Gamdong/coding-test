9528fb9ee5eb1c6571fcd7041c92f2a48fc04d8c
mklee-mac <mklee@headit.co.kr>
2022-05-25 00:34:55 +0900

    특정 문자열과 최소거리 계산

---
+++ src/가장짧은문자거리.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class 가장짧은문자거리 {
+
+    private int[] solution(String s, char t) {
+
+        s = s.toLowerCase();
+        int[] arrLeft = new int[s.length()];
+        int[] arrRight = new int[s.length()];
+        int distanceFromLtc = s.length();
+        int distanceFromRtc = s.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            char lX = s.charAt(i);
+            char rX = s.charAt(s.length() - i - 1);
+
+            if (lX == t) {
+                distanceFromLtc = 0;
+            } else {
+                distanceFromLtc++;
+            }
+            arrLeft[i] = distanceFromLtc;
+
+            if (rX == t) {
+                distanceFromRtc = 0;
+            }
+            else {
+                distanceFromRtc++;
+            }
+            arrRight[s.length() - i - 1] = distanceFromRtc;
+        }
+
+        int[] ans = new int[s.length()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = Math.min(arrLeft[i], arrRight[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        가장짧은문자거리 T = new 가장짧은문자거리();
+        Scanner in = new Scanner(System.in);
+
+        String input = in.next();
+        char find = in.next().charAt(0);
+
+        int[] ans = T.solution(input, find);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ans.length; i++) {
+            int x = ans[i];
+            sb.append(x);
+            if (i < ans.length -1) sb.append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
