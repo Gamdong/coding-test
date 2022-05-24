533b2a1d89a13d1bd2de0859368dc020aa5f7a63
mklee-mac <mklee@headit.co.kr>
2022-05-24 22:25:38 +0900

    숫자만 추출

---
+++ src/숫자만추출.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class 숫자만추출 {
+
+    private int solution(String input) {
+        input = input.replaceAll("[^0-9]", "");
+        return Integer.valueOf(input);
+    }
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+
+        숫자만추출 T = new 숫자만추출();
+        int ans = T.solution(input);
+        System.out.println(ans);
+    }
+}
