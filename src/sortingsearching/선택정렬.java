ef311ba381ad384f8bf2d2ebad32b9dda4687fde
mklee-mac <mklee@headit.co.kr>
2022-06-03 02:03:46 +0900

    정렬

--- src/sortingsearching/선택정렬.java
+++ src/sortingsearching/선택정렬.java
@@ -10,6 +10,18 @@ public class 선택정렬 {
         Arrays.sort(ans);
         return ans;
     }
+    private int[] solution2(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[idx]) idx = j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         선택정렬 T = new 선택정렬();
         Scanner in = new Scanner(System.in);
@@ -18,7 +30,7 @@ public class 선택정렬 {
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) arr[i] = in.nextInt();
 
-        int[] ans = T.solution(arr);
+        int[] ans = T.solution2(arr);
         for (int x : ans) System.out.print(x + " ");
     }
 }
